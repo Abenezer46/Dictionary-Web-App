b47231335ef211fefaa2da0259a815fab8ecc33a
Abenezer46 <75671726+Abenezer46@users.noreply.github.com>
2022-09-13 13:04:14 +0300

    first commit

--- script.js
+++ script.js
@@ -1,25 +1,24 @@
-const container = document.querySelector(".container"),
-  homePage = container.querySelector(".home-page"),
-  themes = homePage.querySelector("header .colors"),
-  searchInput = homePage.querySelector(".input-part input"),
-  hint = homePage.querySelector(".hint"),
-  ul = homePage.querySelector("ul"),
-  speak = ul.querySelector(".word i");
+const container = document.querySelector('.container'),
+  homePage = container.querySelector('.home-page'),
+  themes = homePage.querySelector('header .colors'),
+  searchInput = homePage.querySelector('.input-part input'),
+  hint = homePage.querySelector('.hint'),
+  ul = homePage.querySelector('ul'),
+  speak = ul.querySelector('.word i');
 let audio;
 
-themes.addEventListener('click', e => {
-  console.log(e.target);
-  if (e.target.classList.contains("white")) {
-    document.querySelector("body").classList.add("light");
-  } else if (e.target.classList.contains("dark")) {
-    document.querySelector("body").classList.remove("light");
+themes.addEventListener('click', (e) => {
+  if (e.target.classList.contains('white')) {
+    document.querySelector('body').classList.add('light');
+  } else if (e.target.classList.contains('dark')) {
+    document.querySelector('body').classList.remove('light');
   }
-})
+});
 
-searchInput.addEventListener("keyup", (e) => {
-  if (e.key === "Enter" && e.target.value) {
+searchInput.addEventListener('keyup', (e) => {
+  if (e.keyCode === 13 && e.target.value) {
     fetchApi(e.target.value);
-    e.target.value = "";
+    e.target.value = '';
   }
 });
 
@@ -29,17 +28,17 @@ function fetchApi(word) {
 
   fetch(api)
     .then((response) => response.json())
-    .then((result) => {console.log(result),
+    .then((result) => {
       wordDetails(result, word);
     });
 }
 
 function wordDetails(result, word) {
   if (result.title) {
-    hint.classList.add("error");
+    hint.classList.add('error');
     hint.innerHTML = `Sorry pal, we couldn't find definitions for the word <span>"${word}"</span> you were looking for`;
-    ul.style.display = "none";
-    hint.style.display = "block";
+    ul.style.display = 'none';
+    hint.style.display = 'block';
   } else {
     let phonetic;
     for (let i = 0; i < result[0].phonetics.length; i++) {
@@ -48,19 +47,19 @@ function wordDetails(result, word) {
 
     let partOfSpeech = result[0].meanings[0].partOfSpeech;
 
-    if (ul.querySelector(".content .meaning .details span")) {
+    if (ul.querySelector('.content .meaning .details span')) {
       let meaning = result[0].meanings[0].definitions[0].definition;
 
-      let mean = ul.querySelector(".content .meaning .details span");
+      let mean = ul.querySelector('.content .meaning .details span');
       mean.innerText = meaning;
     } else {
       meaning = result[0].meanings[0].definitions[0].definition;
 
-      let mean = document.createElement("span");
-      mean.classList.add("mean");
-      mean.classList.add("txt");
+      let mean = document.createElement('span');
+      mean.classList.add('mean');
+      mean.classList.add('txt');
       mean.innerText = meaning;
-      ul.querySelector(".content .meaning .details").appendChild(mean);
+      ul.querySelector('.content .meaning .details').appendChild(mean);
     }
 
     let example =
@@ -68,47 +67,47 @@ function wordDetails(result, word) {
 
     audio = result[0].phonetics[0].audio;
     if (audio !== undefined) {
-      speak.addEventListener("click", (e) => {
-          let play = new Audio(audio);
-          play.play();
+      speak.addEventListener('click', (e) => {
+        let play = new Audio(audio);
+        play.play();
       });
     }
 
-    ul.querySelector(".word .details p").innerText = word;
+    ul.querySelector('.word .details p').innerText = word;
     ul.querySelector(
-      ".word .details span"
+      '.word .details span'
     ).innerHTML = `${partOfSpeech} <span>${phonetic}</span>`;
 
     if (example == undefined) {
-      ul.querySelector(".content .example .details .ex").innerText =
-        "no examples found";
+      ul.querySelector('.content .example .details .ex').innerText =
+        'no examples found';
     } else {
-      ul.querySelector(".content .example .details .ex").innerText = example;
+      ul.querySelector('.content .example .details .ex').innerText = example;
     }
 
-    ul.querySelector(".content .synonyms .details .list").innerHTML = "";
+    ul.querySelector('.content .synonyms .details .list').innerHTML = '';
 
     for (let i = 0; i < 4; i++) {
       let tag = result[0].meanings[0].synonyms[i];
       if (tag !== undefined) {
         let synonyms = `<span onClick=search('${tag}') class='synon txt'> ${tag} ,</span>`;
         ul.querySelector(
-          ".content .synonyms .details .list"
-        ).insertAdjacentHTML("beforeend", synonyms);
+          '.content .synonyms .details .list'
+        ).insertAdjacentHTML('beforeend', synonyms);
       } else {
         let synonyms = `<span class='synon txt'> . . . </span>`;
         ul.querySelector(
-          ".content .synonyms .details .list"
-        ).insertAdjacentHTML("beforeend", synonyms);
+          '.content .synonyms .details .list'
+        ).insertAdjacentHTML('beforeend', synonyms);
       }
     }
 
-    ul.style.display = "block";
-    hint.style.display = "none";
+    ul.style.display = 'block';
+    hint.style.display = 'none';
   }
 }
 
 function search(word) {
-    searchInput.value = word;
-    fetchApi(word);
+  searchInput.value = word;
+  fetchApi(word);
 }
